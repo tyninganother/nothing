0426116280a72216e0e64d1c61cf62994dee0f75
tianhaining <haining@hainingdeMacBook-Pro.local>
2018-03-27 17:55:24 +0800

    IoUtil add new methods

--- baseutil/src/test/java/com/tyning/nothing/util/test/UtilTest.java
+++ baseutil/src/test/java/com/tyning/nothing/util/test/UtilTest.java
@@ -1,12 +1,21 @@
 package com.tyning.nothing.util.test;
 
+import org.apache.commons.io.FileUtils;
 import org.junit.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.math.BigDecimal;
 
 public class UtilTest {
 
     @Test
     public void testSerializable(){
-
+//        String projectIdstr = "2";
+//        System.out.println(projectIdstr.replaceAll("1","1"));
+        long a= 11321l;
+        Long b = 111l;
+        System.out.println(b.equals(new Long(a)));
     }
 
 
