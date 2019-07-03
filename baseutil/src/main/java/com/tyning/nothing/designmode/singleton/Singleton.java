57c4272d1963afaea3f383cf0feca2dbc6ae9605
tianhaining <haining@hainingdeMacBook-Pro.local>
2019-07-03 16:45:49 +0800

    nothing

---
+++ baseutil/src/main/java/com/tyning/nothing/designmode/singleton/Singleton.java
@@ -0,0 +1,21 @@
+package com.tyning.nothing.designmode.singleton;
+
+/**
+ * Singleton
+ *
+ * @author haining
+ */
+public class Singleton {
+
+    /* 持有私有静态实例，防止被引用，此处赋值为null，目的是实现延迟加载 */
+    private static Singleton instance = null;
+
+    /* 私有构造方法，防止被实例化 */
+    private Singleton() {
+    }
+
+    /* 如果该对象被用于序列化，可以保证对象在序列化前后保持一致 */
+    public Object readResolve() {
+        return instance;
+    }
+}
