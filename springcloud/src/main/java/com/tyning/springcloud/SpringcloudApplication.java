5f463e5ded435e9ce1716714edfc4ce7d323c9b8
tyninganother <tyning.another@gmail.com>
2020-11-27 16:59:17 +0800

    fixes potential security vulnerabilities

---
+++ springcloud/src/main/java/com/tyning/springcloud/SpringcloudApplication.java
@@ -0,0 +1,15 @@
+package com.tyning.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class SpringcloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringcloudApplication.class, args);
+    }
+
+}
---
+++ springcloud/src/test/java/com/tyning/springcloud/SpringcloudApplicationTests.java
@@ -0,0 +1,11 @@
+package com.tyning.springcloud;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcloudApplicationTests {
+
+	void contextLoads() {
+	}
+
+}
